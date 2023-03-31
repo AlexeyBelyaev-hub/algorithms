c3714848692fb89359a2e6b10bf6dcc8757edda3
Alexey Belyaev <avalbelyaev@sberbank.ru>
2023-03-31 14:43:36 +0300

    [QUEUE] MovingAverage

---
+++ src/queue/MovingAverage.java
@@ -0,0 +1,26 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverage {
+
+    Queue<Integer> q;
+    int window;
+    double sum;
+
+    public MovingAverage(int size) {
+        this.q = new LinkedList<>();
+        this.window = size;
+    }
+
+    public double next(int val) {
+
+        if (q.size() == window)
+            sum -= q.poll();
+
+        sum += val;
+        q.offer(val);
+        return sum/q.size();
+    }
+}
---
+++ src/queue/MovingAverageTest.java
@@ -0,0 +1,16 @@
+package queue;
+
+import org.junit.Test;
+
+
+class MovingAverageTest {
+
+    @Test
+    public void test() {
+        MovingAverage movingAverage = new MovingAverage(3);
+        movingAverage.next(1); // return 1.0 = 1 / 1
+        movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+        movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+        movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+    }
+}
