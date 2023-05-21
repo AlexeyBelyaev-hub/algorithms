9af0a5fafd4bbffcd9b1d5008c8a25bdcfc1726a
Alexey Belyaev <bel.alexey@gmail.com>
2023-05-21 14:01:27 +0300

    [Stack]DailyTemperature - Stack

--- src/stack/temperature/DailyTemperatureStack.java
+++ src/stack/temperature/DailyTemperatureStack.java
@@ -1,26 +1,23 @@
 package stack.temperature;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class DailyTemperatureStack {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<int[]> stack = new Stack<>();
+        Deque<int[]> stack = new ArrayDeque<>();
         int l = temperatures.length;
-        int[]res = new int[l];
-        int count = 0;
-        stack.push(new int[]{temperatures[l-1], count});
-        for (int i = l - 2; i>=1; i--) {
-            if (temperatures[i-1] < temperatures[i]) {
-                count = 1;
-            } else {
-                count++;
+        int[] res = new int[l];
+        stack.push(new int[]{temperatures[0], 0});
+        // Iterate over the array
+        for (int i = 1; i < l; i++) {
+            // Iterate over the stack - there are left only those elements which have non increase temperature
+            while (!stack.isEmpty() && stack.peek()[0] < temperatures[i]) {
+                int prevPos = stack.pop()[1];
+                res[prevPos] = i - prevPos;
             }
-            stack.push(new int[]{temperatures[i], count});
-
-        }
-
-        for (int i = 0; i < l; i++) {
-            res[i] = stack.pop()[1];
+            stack.push(new int[]{temperatures[i], i});
         }
         return res;
     }
